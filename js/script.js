8f7964e3fa82c7f723d4e133eb982ee9b8740f72
mjalowiczor <m.jalowiczor@hotmail.com>
2019-09-03 19:37:42 +0200

    Add score comparison logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (playerInput === '2') {
+    playerMove = 'papier';
+} else {
+    playerMove = 'nożyce';
+}
+
+printMessage('Ruch komputera to: ' + computerMove);
+
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -8,8 +25,22 @@ if (playerInput === '1') {
     playerMove = 'kamień';
 } else if (playerInput === '2') {
     playerMove = 'papier';
-} else {
+} else if (playerInput === '3') {
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove === 'kamień' && playerMove === 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove === 'papier' && playerMove === 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove === 'nożyce' && playerMove === 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove === playerMove) {
+    printMessage('Remis!');
+} else if (playerMove != 'kamień' && playerMove != 'papier' && playerMove != 'nożyce') {
+    printMessage('Wybierz poprawną liczbę od 1 do 3!');
+} else {
+    printMessage('Przegrałeś!');
+}
