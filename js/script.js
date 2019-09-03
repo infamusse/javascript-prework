7ee77ee3cebc184fe22e837fc4a62b5396630579
mjalowiczor <m.jalowiczor@hotmail.com>
2019-09-03 19:07:10 +0200

    Add player choice to variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput === '1') {
+    playerMove = 'kamień';
+} else if (playerInput === '2') {
+    playerMove = 'papier';
+} else {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
